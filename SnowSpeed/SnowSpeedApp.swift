b9a913817f0542ad1ab4a4e13205410dab063091
Xi Huang <xhuang@ucdavis.edu>
2023-03-13 18:23:23 -0700

    Initial Commit

---
+++ SnowSpeed/SnowSpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSpeedApp.swift
+//  SnowSpeed
+//
+//  Created by 黄熙 on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSpeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
